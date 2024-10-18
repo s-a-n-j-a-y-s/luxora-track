146debfc5dd5941369e41f284d63a931e012597d
s-a-n-j-a-y-s <s.sanjaykumarsenthilvel@gmail.com>
2024-10-18 21:46:26 +0530

    LUXT - Code base initalized

---
+++ src/main/java/com/spartan/luxoratrack/LuxoratrackApplication.java
@@ -0,0 +1,13 @@
+package com.spartan.luxoratrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxoratrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxoratrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spartan/luxoratrack/LuxoratrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartan.luxoratrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxoratrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
